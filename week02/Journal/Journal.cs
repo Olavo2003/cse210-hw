a9c3b531efd98c16e2fa1bfa2ce6d24eb8f76a70
Olavo2003 <olavo2003@gmail.com>
2025-09-27 13:42:07 -0300

    Add files via upload

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in entries)
+            {
+                outputFile.WriteLine(entry.ToString());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        if (File.Exists(filename))
+        {
+            entries.Clear();
+            string[] lines = File.ReadAllLines(filename);
+            foreach (string line in lines)
+            {
+                entries.Add(Entry.FromString(line));
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+        }
+    }
+}
